8358d05038c0792495e454db805f428f4e363864
David Kimdon <dkimdon@gmail.com>
2019-11-02 20:04:33 -0700

    State on state API.

--- lambda/index.js
+++ lambda/index.js
@@ -6,9 +6,33 @@ aws.config.update({
 });
 
 exports.lambda_handler = function(event, context, callback) {
-    if (event.field == "ping") {
-        return callback(null, {
-            id: event.arguments.id
+    if (event.field == "get-tub-state") {
+        aws.config.region = 'us-west-2';
+        var iotData = new aws.IotData({
+            endpoint: 'https://a3fqpqsy13jcnc.iot.us-west-2.amazonaws.com'
         });
-    } 
+
+        iotData.getThingShadow({
+            thingName: "HotTub"
+        }, function(err, data) {
+            if (err) {
+                console.log("Error : " + err, err.stack);
+                return callback(err);
+            } else {
+                var shadow = JSON.parse(data.payload);
+                var state = {
+                    lastReportedTemperature: shadow.state.reported.temperature,
+                    lastReportTimestamp: shadow.metadata.reported.temperature.timestamp,
+                    targetTemperature: shadow.state.desired.set_temperature
+                };
+                return callback(null, state);
+            }
+        });
+    }
 }
+
+exports.lambda_handler({
+    field: 'get-tub-state'
+}, null, function(err, res) {
+    console.log(res);
+});
